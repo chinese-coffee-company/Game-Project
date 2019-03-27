17b65fe2a1513f81148daa9a3d7ced90d1725bbe
RainySunset <48899772+RainySunset@users.noreply.github.com>
2019-03-26 22:31:05 -0700

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -1,14 +1,28 @@
 import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
 
 class Frame extends JFrame {
+	
+	private static JLabel t1;
+	
 
   public static void main(String[] args) {
-  Frame f = new Frame();
-  f.setDefaultCloseOperation(EXIT_ON_CLOSE);
-  f.setSize(400,400);
-  f.setVisible(true);
-  f.setResizable(false);
+	  Frame f = new Frame();
+	  f.setTitle("Hello World");
+	  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  f.setSize(400,400);
+	  f.setVisible(true);
+	  f.setResizable(true);
+  
+	  f.setLayout(new FlowLayout());
+  
+	  t1 = new JLabel("Testing 123");
+	  t1.setToolTipText("blah");
+  	  f.add(t1);
+  
 
 
-}
+
+  }
 }
