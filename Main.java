2478c5356395914f094dc0d9574a21147cbd0d73
RainySunset <48899772+RainySunset@users.noreply.github.com>
2019-03-26 21:30:41 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+
+public class Main extends JFrame {
+
+    private JLabel t1;
+    
+    public Main() {
+    super("Test Title");
+    setLayout(new FlowLayOut);
+    
+    t1 = new JLabel("Testing 123");
+    add(t1);
+    
+    }
+  
+
+
+
+
+
+
+
+
+}
