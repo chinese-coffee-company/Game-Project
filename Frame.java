39d13d30d1f3ec0cfc9c66408ca9b03804353dc9
RainySunset <48899772+RainySunset@users.noreply.github.com>
2019-03-26 21:33:09 -0700

    Create Frame.java

---
+++ Frame.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+
+public class Frame extends JFrame {
+
+  public static void main(String[] args) {
+  Frame f = new Frame();
+  f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+  f.setSize(400,400);
+  f.setVisible(true);
+  f.setResizable(false);
+
+
+}
+
+
+
+
+
+}
